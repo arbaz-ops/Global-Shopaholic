4b8b5c15bad71008e35d6ce3f58b701b44416429
arbaz-ops <arbazchughtai55@gmail.com>
2022-02-02 16:08:49 +0500

    no message

--- Global Shopaholic/ViewControllers/ShippedTableViewCell.swift
+++ Global Shopaholic/ViewControllers/ShippedTableViewCell.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ShippedTableViewCell: UITableViewCell {
 
-   
+    @IBOutlet weak var optionButton: UIButton!
+    var optionsView = UITableView()
+    var isShowing: Bool?
     @IBOutlet weak var createdDate: UILabel!
     @IBOutlet weak var requestIdNumber: UILabel!
     @IBOutlet weak var trackingIdNumber: UILabel!
@@ -28,10 +30,40 @@ class ShippedTableViewCell: UITableViewCell {
         // Initialization code
     }
 
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        hideOptions(frame: optionButton.frame)
+
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
     
+    func openOptionsView(frame: CGRect) {
+        optionsView.frame = CGRect(x: frame.origin.x, y: frame.origin.y + frame.height, width: frame.width, height: 0)
+        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut) {[self] in
+            optionsView.isHidden = false
+            optionsView.frame = CGRect(x: (frame.origin.x / 3) * 2, y: frame.origin.y + 30, width: 200, height: 180)
+        } completion: { [self] _ in
+            isShowing = true
+        }
+        
+    }
+    
+    func hideOptions(frame: CGRect) {
+        
+        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut) {[self] in
+            optionsView.frame = CGRect(x: frame.origin.x, y: frame.origin.y + frame.height, width: 0, height: 0)
+        } completion: { [self] _ in
+            isShowing = false
+        }
+
+       
+    }
+    
+    @IBAction func optionButtonTapped(_ sender: UIButton) {
+    }
 }
