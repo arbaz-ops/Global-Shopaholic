6066c15e554f1c17456968fef5dcc961af4c1cbc
arbaz-ops <arbazchughtai55@gmail.com>
2022-02-04 15:05:52 +0500

    no message

--- Global Shopaholic/ViewControllers/SpecialServicesTableViewCell.swift
+++ Global Shopaholic/ViewControllers/SpecialServicesTableViewCell.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class SpecialServicesTableViewCell: UITableViewCell {
 
+    var itemTesting: Bool?
+    var detailedPictures: Bool?
+    var removeItemBoxes: Bool?
+    var removeAllProtectivePackaging: Bool?
+    var removeShipperBoxes: Bool?
+    var removeInvoices: Bool?
+    var prepareInOneBox: Bool?
+    var addFragileSticker: Bool?
+    var useSmallestBox: Bool?
+    var packWithoutBubbleWrap: Bool?
+    var doRemoveItemBoxes: Bool?
+    var addExtraPadding: Bool?
+    var other: Bool?
     @IBOutlet weak var submitButton: UIButton!
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -22,6 +35,62 @@ class SpecialServicesTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    @IBAction func itemTestingCheckBoxTapped(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "checkedGreen"), for: .normal)
+    }
+    @IBAction func detailedPicturesCheckBoxTapped(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "checkedGreen"), for: .normal)
+
+    }
+    @IBAction func removeAllProtectivePackagingTapped(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "checkedGreen"), for: .normal)
+
+    }
+    @IBAction func removeShipperBoxesTapped(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "checkedGreen"), for: .normal)
+
+    }
+    @IBAction func removeItemBoxesTapped(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "checkedGreen"), for: .normal)
+
+    }
+    
+    @IBAction func removeInvoicesTapped(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "checkedGreen"), for: .normal)
+
+    }
+    
+    @IBAction func prepareInOneBoxTapped(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "checkedGreen"), for: .normal)
+
+    }
+    @IBAction func addFragileStickerTapped(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "checkedGreen"), for: .normal)
+
+    }
+    
+    @IBAction func useSmallestBoxTapped(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "checkedGreen"), for: .normal)
+
+    }
+    @IBAction func packWithoutBubbleTapped(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "checkedGreen"), for: .normal)
+
+    }
+    @IBAction func doNotRemoveItemBoxesTapped(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "checkedGreen"), for: .normal)
+
+    }
+    @IBAction func addExtraBubbleTapped(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "checkedGreen"), for: .normal)
+
+    }
+    
+    @IBAction func otherTapped(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "checkedGreen"), for: .normal)
+
     }
     
 }
